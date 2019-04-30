0c99f328c64c38a9407451250b32bcd7b0c8c743
Adriano Di Giovanni <me@adrianodigiovanni.com>
2019-04-30 17:10:27 +0200

    cleanup

--- SwiftRedux/Sources/Subscriber.swift
+++ SwiftRedux/Sources/Subscriber.swift
@@ -1,8 +1,3 @@
-//public protocol Subscriber: class {
-//    associatedtype SubscriberStateType
-//    func update(state: SubscriberStateType)
-//}
-
 public protocol AnySubscriber: class {
     func _update(with state: Any)
 }
